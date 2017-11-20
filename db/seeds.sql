b8a30b14a1ecc43918674055be42289991d3df10
TJ <cooploops@gmail.com>
2017-11-20 10:40:44 -0800

    create db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger, devoured) VALUES ("Bacon Cheese Burger", false);
+INSERT INTO burgers (burger, devoured) VALUES ("Turkey Guacamole Burger", false);
+INSERT INTO burgers (burger, devoured) VALUES ("Five Guys Burger", false);
+INSERT INTO burgers (burger, devoured) VALUES ("Double Double Cheese Burger", false);
