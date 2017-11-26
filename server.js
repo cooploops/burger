733141ef8c2124eb26ebbdda0c3db2eb14d6d76d
TJ <cooploops@gmail.com>
2017-11-26 14:05:10 -0800

    changed port settings for server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const port = process.env.port || 3005;
+const port = process.env.PORT || 3005;
 
 const app = express();
 
